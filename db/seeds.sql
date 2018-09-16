7fb35beac45866bfabd27e4843c9114480975fd0
Jill Morimoto <jillmorimoto@gmail.com>
2018-09-15 20:30:30 -0700

    2

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger Animal Style", true);
+INSERT INTO burgers (burger_name) VALUES ("Mindy Burger");
+INSERT INTO burgers (burger_name) VALUES ("Ramen Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Incredible", true);
+INSERT INTO burgers (burger_name) VALUES ("Fish");
+
+
