020ce721fd234247fb2bb38d4967a59e34dd8429
Jill Morimoto <jillmorimoto@gmail.com>
2018-10-13 18:27:58 -0700

    make it better

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "cig4l2op6r0fxymw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "water",
-    database: "burgers_db"
+    user: "rexx9f6j87liiaee",
+    password: "	jfccc9sibv5qow53",
+    database: "ci9gdlpyg3ld5stu"
   });
 };
 
